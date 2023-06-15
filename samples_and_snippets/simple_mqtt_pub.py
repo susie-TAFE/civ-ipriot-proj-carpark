8dc0755458e6801f515849b1877c03c10d9783cd
Susie ONeil <susie@oneil.com.au>
2023-06-15 22:38:04 +0800

    Modified simple_mqtt_pub.py to publish a message to Display class. To test mqtt, topic and display format.

--- samples_and_snippets/simple_mqtt_pub.py
+++ samples_and_snippets/simple_mqtt_pub.py
@@ -6,3 +6,6 @@ client = paho.Client()
 client.connect(BROKER, PORT)
 client.publish("lot/sensor", "Hello from Python")
 
+# test that Display is receiving and correctly formatting a message.
+message_for_display = "Available Spaces: 196,Temperature: 23C,Current Time: 09:00"
+client.publish("display", message_for_display)
