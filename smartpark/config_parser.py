7b615918fa7201eaa913134f24d9915f5089aada
Susie ONeil <susie@oneil.com.au>
2023-06-16 11:42:58 +0800

    Addressed TODO in config_parser.py. "get the configuration from a parsed file"

--- smartpark/config_parser.py
+++ smartpark/config_parser.py
@@ -1,39 +1,6 @@
-"""A class or function to parse the config file and return the values as a dictionary.
-
-The config file itself can be any of the following formats:
-
-- ryo: means 'roll your own' and is a simple text file with key-value pairs separated by an equals sign. For example:
-```
-location = "Moondalup City Square Parking"
-number_of_spaces = 192
-```
-**you** read the file and parse it into a dictionary.
-- json: a json file with key-value pairs. For example:
-```json
-{location: "Moondalup City Square Parking", number_of_spaces: 192}
-```
-json is built in to python, so you can use the json module to parse it into a dictionary.
-- toml: a toml file with key-value pairs. For example:
-```toml
-[location]
-name = "Moondalup City Square Parking"
-spaces = 192
-```
-toml is part of the standard library in python 3.11, otherwise you need to install tomli to parse it into a dictionary.
-```bash
-python -m pip install tomli
-```
-see [realpython.com](https://realpython.com/python-toml/) for more info.
-
-Finally, you can use `yaml` if you prefer.
-
-
-
-"""
-
-
-
-def parse_config(config: dict) -> dict:
-    """Parse the config file and return the values as a dictionary"""
-    # TODO: get the configuration from a parsed file
-    return {'location': 'TBD', 'total_spaces': 0, 'broker_host': 'TBD', 'broker_port': 0}
+def parse_config(file_name: str) -> dict:
+    """Parse the config file as a string and return the values as a dictionary"""
+    import tomli
+    with open(file_name, "rb") as config_file:
+        config_data = tomli.load(config_file)
+    return config_data
