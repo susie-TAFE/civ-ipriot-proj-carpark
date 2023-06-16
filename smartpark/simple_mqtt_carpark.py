0992babe9d2fa79d04c81585d5c22d86090e7c36
Susie ONeil <susie@oneil.com.au>
2023-06-16 15:28:26 +0800

    Created car-park_config.toml and addressed TODO in simple_mqtt_carpark.py. "Read config from file".

--- smartpark/simple_mqtt_carpark.py
+++ smartpark/simple_mqtt_carpark.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from config_parser import parse_config
 
 import mqtt_device
 import paho.mqtt.client as paho
@@ -67,17 +68,8 @@ class CarPark(mqtt_device.MqttDevice):
 
 
 if __name__ == '__main__':
-    config = {'name': "raf-park",
-              'total-spaces': 130,
-              'total-cars': 0,
-              'location': 'L306',
-              'topic-root': "lot",
-              'broker': 'localhost',
-              'port': 1883,
-              'topic-qualifier': 'entry',
-              'is_stuff': False
-              }
-    # TODO: Read config from file
+    config_file = 'car-park_config.toml'
+    config = parse_config(config_file)
     car_park = CarPark(config)
     print("Carpark initialized")
     print("Carpark initialized")
