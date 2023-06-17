b7f660bec0cb4ae2fa0a862f1cd5b87bfc48b616
Susie ONeil <susie@oneil.com.au>
2023-06-17 15:50:02 +0800

    Changed test_config.py to test for parsing a file. Added copy of config.toml to tests directory to avoid path issues.

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,20 +1,11 @@
 import unittest
 
-import tomli  # you can use toml, json,yaml, or ryo for your config file
-
 import smartpark.config_parser as pc
 
 
 class TestConfigParsing(unittest.TestCase):
     def test_parse_config_has_correct_location_and_spaces(self):
-        config_string = '''
-        [parking_lot]
-        location = "Moondalup City Square Parking"
-        total_spaces = 192
-        broker_host = "localhost"
-        broker_port = 1883
-        '''
-        config = tomli.loads(config_string)
-        parking_lot = pc.parse_config(config)
-        self.assertEqual(parking_lot['location'], "Moondalup City Square Parking")
-        self.assertEqual(parking_lot['total_spaces'], 192)
+        test_file = "test_config.toml"
+        parking_lot = pc.parse_config(test_file)
+        self.assertEqual(parking_lot['parking_lot']['location'], "Moondalup City Square Parking")
+        self.assertEqual(parking_lot['parking_lot']['total_spaces'], 192)
